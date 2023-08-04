0d356445a3a51ffd14a76256817cf7644723cd89
bkarthiklal <bkarthiklal@gmail.com>
2023-08-04 11:51:30 +0530

    Removing inbuild scaffolding and creating a blank slate

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,26 @@
 <template>
   <section class="section">
     <div class="columns is-mobile">
-      <card title="Free" icon="github">
-        Open source on <a href="https://github.com/buefy/buefy"> GitHub </a>
-      </card>
-
-      <card title="Responsive" icon="cellphone-link">
-        <b class="has-text-grey"> Every </b> component is responsive
-      </card>
-
-      <card title="Modern" icon="alert-decagram">
-        Built with <a href="https://vuejs.org/"> Vue.js </a> and
-        <a href="http://bulma.io/"> Bulma </a>
-      </card>
-
-      <card title="Lightweight" icon="arrange-bring-to-front">
-        No other internal dependency
-      </card>
+      <div class="column">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos a
+        sint fugit impedit facilis, quasi molestiae, enim incidunt perferendis
+        sit veniam iure molestias eligendi. Nam omnis, voluptatum vitae cumque
+        facilis praesentium fuga ducimus dolore iste accusamus ipsam aut in
+        architecto, mollitia autem dolores dolorum, doloremque quod voluptatibus
+        labore iusto delectus quis? At earum eaque, mollitia sequi reprehenderit
+        voluptatibus. Sequi maiores soluta, praesentium ipsam quo mollitia.
+        Adipisci, dignissimos ullam? Dignissimos, sequi saepe magnam debitis
+        temporibus aspernatur, id ducimus perferendis maxime inventore
+        consequatur mollitia doloribus aut quae dolor. Illo exercitationem
+        ratione possimus voluptatem eveniet ab ducimus, nulla quis a? Quis,
+        mollitia velit!
+      </div>
     </div>
   </section>
 </template>
 
 <script>
-import Card from '~/components/Card'
-
 export default {
   name: 'IndexPage',
-  components: {
-    Card,
-  },
 }
 </script>
