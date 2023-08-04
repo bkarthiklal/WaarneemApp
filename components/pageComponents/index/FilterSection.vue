adbbeb4ed32fdbf3b0acd9a76727f8f8e068dd86
bkarthiklal <bkarthiklal@gmail.com>
2023-08-04 12:19:07 +0530

    Created a component for filter section of landing page

---
+++ components/pageComponents/index/FilterSection.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="columns">
+    <div class="column">
+      <h2 class="is-size-2"><b> Filter </b></h2>
+      <div class="columns">
+        <div class="column">
+          <b-field>
+            <b-slider v-model="numbers" :min="1" :max="15" :step="0.5" ticks>
+            </b-slider>
+          </b-field>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FilterSection',
+  data() {
+    return {
+      numbers: [2, 5],
+    }
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,147 +1,18 @@
 <template>
   <section class="section pb-1" :class="open ? 'sidebar-open' : ''">
-    <div class="columns">
-      <div class="column">
-        <h2 class="is-size-2"><b> Heading </b></h2>
-        <h2 class="is-size-2">
-          <b>
-            <b-button type="is-dark" @click="open = true" class="is-clickable"
-              >Show</b-button
-            >
-          </b>
-        </h2>
-        <div class="columns">
-          <div class="column">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem,
-            itaque! Incidunt impedit sapiente, vitae reprehenderit ratione
-            adipisci? Commodi laudantium incidunt reiciendis provident tenetur
-            placeat maiores! A dicta velit fugiat at iste? Explicabo nesciunt
-            quis asperiores tenetur quam praesentium fuga cupiditate distinctio
-            commodi quae sit quidem numquam sequi quas, facere veniam at
-            sapiente minima temporibus natus est similique! Magnam eum et
-            expedita quidem praesentium molestiae, perferendis saepe sint, earum
-            voluptatum qui amet? Ab ex, est beatae magni quisquam adipisci sunt?
-            Earum itaque recusandae vero et quia at tempora labore voluptatibus
-            accusamus non necessitatibus consectetur aspernatur enim velit animi
-            excepturi est, iste ipsa similique doloremque architecto hic
-            aliquam, asperiores officiis! Suscipit est itaque dolore iusto sit
-            ad ipsam perspiciatis, temporibus animi sed, obcaecati optio quae
-            blanditiis corporis quo. Velit, quae. Consectetur illo dolorem
-            molestias culpa aliquid porro quasi explicabo corrupti amet nostrum.
-            Perferendis quidem tempore non veritatis harum numquam? Quaerat
-            nihil distinctio deleniti unde maxime. Maxime iste blanditiis ipsam
-            magni rem inventore. Inventore atque suscipit temporibus nulla vel
-            aut voluptatum quasi excepturi saepe placeat. Facere nihil, mollitia
-            rerum voluptatem atque corporis aliquam illo reiciendis praesentium
-            delectus nulla at doloribus iusto fuga, quibusdam necessitatibus.
-            Vitae quaerat voluptates quas. Libero, inventore voluptate eum a
-            alias assumenda at voluptates necessitatibus corporis asperiores
-            doloremque quis ad aut sint repellendus id cupiditate deserunt
-            autem, culpa repellat exercitationem hic iusto ratione cum?
-            Accusantium ratione suscipit aut voluptatum? Cum magni unde sit
-            voluptates ipsum ex aliquam optio voluptatibus in beatae laboriosam
-            cumque quae eaque nulla quisquam magnam repellendus minus quis
-            consequatur expedita harum corrupti, amet soluta praesentium!
-            Suscipit placeat aut explicabo reprehenderit laborum tenetur eos
-            architecto velit molestias maxime, deserunt cumque earum eum error
-            porro non illo ab rem officia odit distinctio accusamus quasi
-            cupiditate sunt. Nobis reiciendis quisquam pariatur unde? Optio
-            aperiam libero voluptatibus alias voluptate, numquam nam saepe
-            nulla, delectus blanditiis repellat reprehenderit ipsam accusantium.
-            Similique voluptatum nam minus debitis laudantium pariatur iure
-            error, aut magni repellendus, harum molestiae. Molestiae, maxime
-            asperiores, optio culpa nostrum mollitia ab quia suscipit eligendi
-            facilis distinctio voluptatem dolores nihil vero accusantium,
-            molestias non soluta. Dolorem sint ut vero reprehenderit, quod iste
-            quis accusamus corrupti possimus molestias modi facilis vitae
-            laboriosam doloremque! Suscipit omnis, pariatur necessitatibus non
-            blanditiis sint ipsum corrupti quas maiores hic tempore aliquam
-            porro fugiat recusandae vitae perferendis inventore, rerum eius quae
-            quos explicabo aperiam itaque aspernatur. Maxime voluptas nesciunt
-            nisi reiciendis optio deleniti. Maxime fugit et accusantium enim
-            corporis sed, natus repudiandae ipsum ducimus blanditiis vero autem
-            amet assumenda incidunt porro numquam in earum? Optio quibusdam
-            reiciendis ipsum voluptatum minima est aliquam, saepe impedit earum
-            excepturi porro fugit architecto quaerat consequuntur in delectus,
-            aperiam laudantium facilis ab cum incidunt a autem hic rem. Aliquid,
-            vitae? Sapiente nostrum adipisci ex nam eius facilis tenetur,
-            nesciunt doloribus corporis, voluptatem officiis eaque delectus ut
-            voluptatum? Quibusdam dolores, vel quasi quos, quod laboriosam
-            laudantium id repudiandae qui numquam molestiae tempora accusantium
-            excepturi nostrum facilis esse illum odit quas provident possimus.
-            Provident voluptate modi temporibus laborum, enim fuga facilis
-            accusamus vero esse quam odio? Voluptate, officia maxime! Rerum,
-            fugit harum. Accusamus fugit minus non similique quia voluptates
-            voluptas atque vero nihil repellendus! Quo eveniet harum, debitis
-            illum, numquam omnis provident sit cupiditate at et error iste nemo
-            eius officiis quidem obcaecati. Sit debitis dicta minima itaque,
-            sint voluptas natus maxime earum minus obcaecati animi velit
-            aspernatur dolores magnam quisquam cum, nam eligendi? In, ex
-            voluptatum temporibus eius cumque perspiciatis numquam, assumenda
-            obcaecati illo cum ducimus delectus sint debitis amet ratione neque
-            minima soluta est dolor? Maiores labore assumenda libero reiciendis!
-            Quos commodi culpa beatae? Praesentium molestiae sunt dolorem quis
-            cum itaque, modi repudiandae officiis facere beatae accusamus eius
-            reiciendis labore quo excepturi inventore eaque! Non ratione facere
-            similique, quae doloribus cupiditate eligendi est. Voluptatem non
-            velit tempore repellat pariatur facilis, asperiores laboriosam
-            libero harum quibusdam veniam earum tempora itaque, corrupti sed
-            molestiae accusamus perspiciatis consectetur, unde officiis quisquam
-            neque minus! Consectetur fugit nostrum saepe placeat atque
-            temporibus, neque id quas voluptatum aliquid maxime omnis quaerat
-            nemo fuga nisi, corrupti tempora et? Mollitia quo alias consectetur
-            nisi officiis ipsam, corrupti totam temporibus illum magnam odit
-            porro ratione dolore accusantium rerum quos! Animi saepe error iure
-            earum doloribus provident magni unde, beatae natus voluptas pariatur
-            quo voluptatem rem delectus eligendi at perferendis praesentium
-            quibusdam ad tempore autem dolorem quasi. Iure repellat, incidunt
-            veniam numquam iste odit magni voluptatibus? Voluptas facilis
-            architecto nesciunt corrupti, perferendis beatae. Ducimus,
-            voluptatem. Expedita voluptas dolorem ad ea error ratione aliquid
-            minima, minus iusto omnis iure pariatur id cupiditate quod illum
-            nemo non molestias nostrum sint. Odit, doloribus, explicabo in ipsam
-            tempora velit rerum odio inventore, consequuntur temporibus suscipit
-            ex facilis a vel unde cumque provident excepturi beatae vitae.
-            Reprehenderit voluptates, ratione sed praesentium illum debitis quae
-            eum dolores accusamus aspernatur omnis, ex ullam reiciendis quos
-            cupiditate voluptate. Dolorum eaque corporis possimus quisquam
-            reiciendis quia quaerat fugiat repellendus quod. Reprehenderit ab
-            aperiam sed labore, obcaecati fugit, accusantium voluptatum atque,
-            nostrum nulla fugiat temporibus possimus. Ipsa ratione distinctio,
-            corporis modi totam cumque nemo vel assumenda deserunt at enim
-            numquam mollitia soluta? Porro ab dignissimos quo deleniti esse. At
-            perspiciatis, iusto voluptates nihil atque porro rerum maxime
-            assumenda non tempora officia dignissimos in, quo qui alias ducimus
-            ex vel repellendus soluta asperiores. Omnis nihil saepe accusantium
-            at sapiente quia sint, aspernatur dolorem dicta culpa minima,
-            laboriosam sunt blanditiis tenetur perspiciatis asperiores deleniti
-            adipisci dolor ut aliquid qui dolorum repudiandae enim? Minus non
-            totam, a nam fuga voluptate sed rem quia? Quisquam tempore pariatur
-            iure in nemo quibusdam deserunt debitis similique reprehenderit
-            assumenda aperiam commodi veritatis distinctio quasi eum fugiat
-            recusandae est, accusamus sunt unde. Quibusdam eligendi inventore
-            excepturi similique! Vero nostrum eligendi ut modi sed odio quasi
-            optio repellat reiciendis facere quam sequi, debitis praesentium
-            pariatur labore hic cupiditate at fuga ratione fugit! Quam, est
-            ullam. Numquam neque corporis esse amet, magnam autem eaque veniam
-            perspiciatis placeat suscipit temporibus tempora voluptatem cum sed
-            facilis quidem illo, pariatur ipsam repudiandae laboriosam?
-            Accusamus facere illum sit numquam impedit, perspiciatis quibusdam
-            officia et in magni tenetur?
-          </div>
-        </div>
-      </div>
-    </div>
+    <FilterSection></FilterSection>
     <SideBar v-model="open"></SideBar>
   </section>
 </template>
 
 <script>
 import SideBar from '@/components/SideBar.vue'
+import FilterSection from '@/components/pageComponents/index/FilterSection.vue'
 export default {
   name: 'IndexPage',
   components: {
     SideBar,
+    FilterSection,
   },
   data() {
     return {
